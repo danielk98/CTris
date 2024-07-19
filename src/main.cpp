b24fe58e5d9a8f879c9f2936e267e9097d0b9d62
danielk98 <danielkarl-1998@gmx.de>
2024-07-19 23:55:45 +0200

    add time measurement

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,8 @@
 #include "windows/game_window.h"
 #include "../include/constants.h"
 #include "../include/all_blocks.h"
+#include <iostream>
+#include <chrono>
 
 constexpr void initColors();
 bool canMoveRight(Block block, GameWindow &win);
@@ -48,6 +50,15 @@ int main(void)
 	int downTimer = 0; //Once this hits a certain number of loops we move the block down
 	int userInput;
 	bool gameOver = false;
+
+
+	clock_t start_time = std::clock();
+	double ret = 0;
+	for (int i=0; i<0xFFFFF; i++) { ret += ret*0.3; } // task to be measured
+	clock_t end_time = std::clock();
+	float diff = static_cast<float>(end_time - start_time); // static_cast
+	diff /= CLOCKS_PER_SEC; // POSIX-defined as 1000000
+	std::cout << "Time: " << 1000*diff << " milliseconds \n";
 
 	//Game loop
 	for (;;)
